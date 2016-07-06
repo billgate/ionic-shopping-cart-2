8d037bca2116334c41c6f92576c39d82f0469813
Arjun SK <arjunsk@users.noreply.github.com>
2016-07-06 17:04:27 +0530

    I have reverted the changes.

--- server_side/signup.php
+++ server_side/signup.php
@@ -6,8 +6,6 @@
         header('Access-Control-Max-Age: 86400');    // cache for 1 day
     }
 
- 
-
     // Access-Control headers are received during OPTIONS requests
     if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
@@ -15,17 +13,16 @@
 
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
             header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+			
         exit(0);
     }
 
-
-
- 
-
    $postdata = file_get_contents("php://input");
-
+   
   if (isset($postdata)) {
+
 		$request = json_decode($postdata);
+	
 		$name=$request->n;  
 		$username=$request->un; 		
 		$password=$request->ps; 
@@ -33,38 +30,50 @@
 		$phone=$request->ph; 
 		$address=$request->add; 
 		$pincode=$request->pin; 
-		
+
 		$conn = new mysqli('localhost', 'user', 'pass', 'dbname');
 
 		// To protect MySQL injection for Security purpose
+
 		$name = stripslashes($name);
 		$username = stripslashes($username);
 		$password = stripslashes($password);
 		$phone = stripslashes($phone);
 		$address = stripslashes($address);
-	        $pincode = stripslashes($pincode);
-		// the above stripslashes is not required unless if you are using javascript, which im still new to Ionic but i guess it uses javascript for posting ? So i will just keep them.
+		$pincode = stripslashes($pincode);
 
-		$stment = $conn->prepare("SELECT count(*) FROM `users` WHERE `u_id` = ?");
-		$stment->bind_param('s', $username);
-		$stment->execute();
+		
 
-		$return = $stment->fetch();
-		if ($return) {
+		$name = $conn->real_escape_string($name);
+		$username = $conn->real_escape_string($username);
+		$password = $conn->real_escape_string($password);
+		$phone = $conn->real_escape_string($phone);
+		$address = $conn->real_escape_string($address);
+		$pincode = $conn->real_escape_string($pincode);
+
+		
+
+		$check="SELECT count(*) FROM users WHERE u_id = '$username'";
+		$rs = mysqli_query($conn,$check);
+		$data = mysqli_fetch_array($rs, MYSQLI_NUM);
+
+		//print_r($data);
+
+		if($data[0] > 0) {
 			$outp='{"result":{"created": "0" , "exists": "1" } }';
-		} else {
-			$stment = $conn->prepare("INSERT INTO users (u_name, u_id, u_password, u_phone, u_address, u_pincode, u_verified) VALUES (?, ?, ?, ?, ?, ?, ?)");
-			$stment->bind_param("ssssssi", $name, $username, $password, $phone, &address, $pincode, 1);
-			$stment->execute();
-			$outp='{"result":{"created": "1", "exists": "0" } }';
 		}
 
+		else{	
 
-		echo $outp;
+			$sql = "INSERT INTO users VALUES ('$name', '$username', '$password', '$phone','$address' ,'$pincode',1 )";		
+			if ($conn->query($sql) === TRUE) {
+				$outp='{"result":{"created": "1", "exists": "0" } }';
+			} 
+
+		}
 		
-		$stment-close();
-		$conn->close();
-
+		echo $outp;
+		$conn->close();	
 }
 
 ?> 
